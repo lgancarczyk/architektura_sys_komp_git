e892cb7fe6f8c921f22d0d9493e20792f8e4e68c
lgancarczyk <lukgan2000@gmail.com>
2021-03-12 15:52:33 +0100

    RadioButtons

--- projekt/main.py
+++ projekt/main.py
@@ -4,31 +4,36 @@ root = Tk()
 root.resizable(width=0, height=0)
 root.title("8086 simulator")
 
+var_left= IntVar()
+var_right= IntVar()
 
 large_font = ('Verdana',15)
+middle_font = ('Verdana',11)
 small_font = ('Verdana',10)
+
+###########################  Output
 
 AX_output_desc = Label(root, text="AX", font=small_font)
 AX_output_desc.grid(row=1,column=1, padx=(20,0), pady=(20, 0))
-AX_output = Entry(root, font=large_font, width=6, justify='center')
+AX_output = Entry(root, font=large_font, width=6, justify='center', state="disabled")
 AX_output.grid(row=1, column=2, padx=(0,0), pady=(20, 0))
 
 BX_output_desc = Label(root, text="BX", font=small_font)
 BX_output_desc.grid(row=3,column=1, padx=(20,0), pady=(20, 0))
-BX_output = Entry(root, font=large_font, width=6, justify='center')
+BX_output = Entry(root, font=large_font, width=6, justify='center', state="disabled")
 BX_output.grid(row=3, column=2, padx=(0,0), pady=(20, 0))
 
 CX_output_desc = Label(root, text="CX", font=small_font)
 CX_output_desc.grid(row=5,column=1, padx=(20,0), pady=(20, 0))
-CX_output = Entry(root, font=large_font, width=6, justify='center')
+CX_output = Entry(root, font=large_font, width=6, justify='center', state="disabled")
 CX_output.grid(row=5, column=2, padx=(0,0), pady=(20, 0))
 
 DX_output_desc = Label(root, text="DX", font=small_font)
 DX_output_desc.grid(row=7,column=1, padx=(20,0), pady=(20, 0))
-DX_output = Entry(root, font=large_font, width=6, justify='center')
+DX_output = Entry(root, font=large_font, width=6, justify='center', state="disabled")
 DX_output.grid(row=7, column=2, padx=(0,0), pady=(20, 0))
 
-#############################
+#############################  Input left
 
 AH_entry_desc = Label(root, text="AH")
 AH_entry_desc.grid(row=1,column=4, padx=(20,0), pady=(20, 0))
@@ -50,7 +55,7 @@ DH_entry_desc.grid(row=7,column=4, padx=(20,0), pady=(20, 0))
 DH_entry = Entry(root, font=large_font, width=3, justify='center')
 DH_entry.grid(row=7, column=5, padx=(0,0), pady=(20, 0))
 
-#############################
+#############################  Input Right
 
 AL_entry_desc = Label(root, text="AL")
 AL_entry_desc.grid(row=1,column=7, padx=(20,0), pady=(20, 0))
@@ -72,18 +77,49 @@ DL_entry_desc.grid(row=7,column=7, padx=(20,0), pady=(20, 0))
 DL_entry = Entry(root, font=large_font, width=3, justify='center')
 DL_entry.grid(row=7, column=8, padx=(0,20), pady=(20, 0))
 
-###################################
+###################################  Radio Button left
 
-AX_btn_left_desc = Label(root, text="AX")
+AX_btn_left_desc = Label(root, text="AX", font=middle_font)
 AX_btn_left_desc.grid(row=10,column=1, padx=(20,0), pady=(40, 0))
-AX_btn_left = Radiobutton(root)
+AX_btn_left = Radiobutton(root, variable=var_left, value=0)
 AX_btn_left.grid(row=10, column=2, padx=(0,0), pady=(40, 0), sticky="w")
 
-BX_btn_desc = Label(root, text="BX")
-BX_btn_desc.grid(row=12,column=1, padx=(20,0), pady=(3, 0))
-BX_btn = Radiobutton(root)
-BX_btn.grid(row=12, column=2, padx=(0,0), pady=(3, 0), sticky="w")
+BX_btn_left_desc = Label(root, text="BX", font=middle_font)
+BX_btn_left_desc.grid(row=12,column=1, padx=(20,0), pady=(3, 0))
+BX_btn_left = Radiobutton(root, variable=var_left, value=1)
+BX_btn_left.grid(row=12, column=2, padx=(0,0), pady=(3, 0), sticky="w")
 
+BX_btn_left_desc = Label(root, text="CX", font=middle_font)
+BX_btn_left_desc.grid(row=14,column=1, padx=(20,0), pady=(3, 0))
+BX_btn_left = Radiobutton(root, variable=var_left, value=2)
+BX_btn_left.grid(row=14, column=2, padx=(0,0), pady=(3, 0), sticky="w")
+
+BX_btn_left_desc = Label(root, text="DX", font=middle_font)
+BX_btn_left_desc.grid(row=16,column=1, padx=(20,0), pady=(3, 0))
+BX_btn_left = Radiobutton(root, variable=var_left, value=3)
+BX_btn_left.grid(row=16, column=2, padx=(0,0), pady=(3, 0), sticky="w")
+
+#########################################  Radio Button right
+
+AX_btn_right_desc = Label(root, text="AX", font=middle_font)
+AX_btn_right_desc.grid(row=10,column=3, padx=(0,0), pady=(40, 0))
+AX_btn_right = Radiobutton(root, variable=var_right, value=0)
+AX_btn_right.grid(row=10, column=4, padx=(0,0), pady=(40, 0), sticky="w")
+
+BX_btn_right_desc = Label(root, text="BX", font=middle_font)
+BX_btn_right_desc.grid(row=12,column=3, padx=(0,0), pady=(3, 0))
+BX_btn_right = Radiobutton(root, variable=var_right, value=1)
+BX_btn_right.grid(row=12, column=4, padx=(0,0), pady=(3, 0), sticky="w")
+
+CX_btn_right_desc = Label(root, text="CX", font=middle_font)
+CX_btn_right_desc.grid(row=14,column=3, padx=(0,0), pady=(3, 0))
+CX_btn_right = Radiobutton(root, variable=var_right, value=2)
+CX_btn_right.grid(row=14, column=4, padx=(0,0), pady=(3, 0), sticky="w")
+
+DX_btn_right_desc = Label(root, text="DX", font=middle_font)
+DX_btn_right_desc.grid(row=16,column=3, padx=(0,0), pady=(3, 0))
+DX_btn_right = Radiobutton(root, variable=var_right, value=3)
+DX_btn_right.grid(row=16, column=4, padx=(0,0), pady=(3, 0), sticky="w")
 
 
 
